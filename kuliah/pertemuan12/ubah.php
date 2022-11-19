fba1b181493f4647f1bdfa95959f1126df7504fc
dickyyahya <dickyyahya25@gmail.com>
2022-11-20 06:58:25 +0700

    login durung bisa bagian password verify

---
+++ kuliah/pertemuan12/ubah.php
@@ -0,0 +1,90 @@
+<?php
+session_start();
+if (!isset($_SESSION['login'])) {
+  header('Location: login.php');
+  exit;
+}
+
+require 'functions.php';
+
+
+//jika tidak ada id di url
+if (!isset($_GET['id'])) {
+  header("Location: index.php");
+  exit;
+}
+
+//ambil id dari url
+$id = $_GET['id'];
+
+//query mahasiswa berdasarkan id
+$m = query("SELECT * FROM mahasiswa WHERE id = $id");
+
+
+
+//cek apakah tombol ubah sudah ditekan
+if (isset($_POST['ubah'])) {
+  if (ubah($_POST) > 0) {
+    echo "<script>
+            alert('data berhasil ubah');
+            document.location.href = 'index.php';
+          </script>";
+  } else {
+    echo "data gagal ubah";
+  }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Ubah Data Mahasiswa </title>
+</head>
+
+<body>
+  <h3>Form Ubah Data mahasiswa</h3>
+  <form action="" method="post">
+    <input type="hidden" name="id" value="<?= $m['id']; ?>">
+    <ul>
+      <li>
+        <label>
+          Nama :
+          <input type="text" name="nama" autofocus required value="<?= $m['nama']; ?>">
+        </label>
+      <li>
+        <label>
+          NIM :
+          <input type="text" name="nim" required value="<?= $m['nim']; ?>">
+        </label>
+      </li>
+      <li>
+        <label>
+          Email :
+          <input type="text" name="email" required value="<?= $m['email']; ?>">
+        </label>
+      </li>
+      <li>
+        <label>
+          Jurusan :
+          <input type="text" name="jurusan" required value="<?= $m['jurusan']; ?>">
+        </label>
+      </li>
+      <li><label>
+          Gambar :
+          <input type="text" name="gambar" required value="<?= $m['gambar']; ?>">
+        </label>
+      </li>
+      <li>
+        <button type="submit" name="ubah"> Ubah data </button>
+      </li>
+      </li>
+    </ul>
+  </form>
+
+
+</body>
+
+</html>
